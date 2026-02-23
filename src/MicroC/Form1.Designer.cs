6ce7f94f9c1d94184d4aa50ec52c386ea58fd810
Angello Contreras <angello.contreras29@gmail.com>
2026-02-22 22:06:51 -0600

    feat: diseñar interfaz principal con editor, consola y menú

--- src/MicroC/Form1.Designer.cs
+++ src/MicroC/Form1.Designer.cs
@@ -28,12 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            crownMenuStrip1 = new ReaLTaiizor.Controls.CrownMenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            nuevoToolStripMenuItem = new ToolStripMenuItem();
+            abrirToolStripMenuItem = new ToolStripMenuItem();
+            guardarToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            editarToolStripMenuItem = new ToolStripMenuItem();
+            compilarToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            richTextBox1 = new RichTextBox();
+            crownMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // crownMenuStrip1
+            // 
+            crownMenuStrip1.BackColor = Color.FromArgb(60, 63, 65);
+            crownMenuStrip1.ForeColor = Color.FromArgb(220, 220, 220);
+            crownMenuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, editarToolStripMenuItem, compilarToolStripMenuItem, ayudaToolStripMenuItem });
+            crownMenuStrip1.Location = new Point(0, 0);
+            crownMenuStrip1.Name = "crownMenuStrip1";
+            crownMenuStrip1.Padding = new Padding(3, 2, 0, 2);
+            crownMenuStrip1.Size = new Size(800, 24);
+            crownMenuStrip1.TabIndex = 0;
+            crownMenuStrip1.Text = "crownMenuStrip1";
+            crownMenuStrip1.ItemClicked += crownMenuStrip1_ItemClicked;
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { nuevoToolStripMenuItem, abrirToolStripMenuItem, guardarToolStripMenuItem, salirToolStripMenuItem });
+            archivoToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            nuevoToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            nuevoToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            nuevoToolStripMenuItem.Size = new Size(116, 22);
+            nuevoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            abrirToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            abrirToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            abrirToolStripMenuItem.Size = new Size(116, 22);
+            abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            guardarToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            guardarToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            guardarToolStripMenuItem.Size = new Size(116, 22);
+            guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            salirToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(116, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // editarToolStripMenuItem
+            // 
+            editarToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            editarToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            editarToolStripMenuItem.Size = new Size(49, 20);
+            editarToolStripMenuItem.Text = "Editar";
+            // 
+            // compilarToolStripMenuItem
+            // 
+            compilarToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            compilarToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            compilarToolStripMenuItem.Name = "compilarToolStripMenuItem";
+            compilarToolStripMenuItem.Size = new Size(68, 20);
+            compilarToolStripMenuItem.Text = "Compilar";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.BackColor = Color.FromArgb(60, 63, 65);
+            ayudaToolStripMenuItem.ForeColor = Color.FromArgb(220, 220, 220);
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(richTextBox1);
+            splitContainer1.Size = new Size(800, 596);
+            splitContainer1.SplitterDistance = 266;
+            splitContainer1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.FromArgb(60, 63, 65);
+            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.ForeColor = SystemColors.ControlLight;
+            richTextBox1.Location = new Point(0, 0);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(266, 596);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 620);
+            Controls.Add(splitContainer1);
+            Controls.Add(crownMenuStrip1);
+            MainMenuStrip = crownMenuStrip1;
+            Name = "Form1";
             Text = "Form1";
+            crownMenuStrip1.ResumeLayout(false);
+            crownMenuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ReaLTaiizor.Controls.CrownMenuStrip crownMenuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem nuevoToolStripMenuItem;
+        private ToolStripMenuItem abrirToolStripMenuItem;
+        private ToolStripMenuItem guardarToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem editarToolStripMenuItem;
+        private ToolStripMenuItem compilarToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private SplitContainer splitContainer1;
+        private RichTextBox richTextBox1;
     }
 }
--- src/MicroC/Form1.cs
+++ src/MicroC/Form1.cs
@@ -1,10 +1,17 @@
+using ReaLTaiizor.Forms;
+
 namespace MicroC
 {
-    public partial class Form1 : Form
+    public partial class Form1 : CrownForm
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void crownMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
         }
     }
 }
